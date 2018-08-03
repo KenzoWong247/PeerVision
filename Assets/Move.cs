b905b1acf98efa17b6b95e61a0795bc7739261a2
Draper <panirwalak@wit.edu>
2018-08-03 12:53:58 -0700

    Added test cube

---
+++ Assets/Move.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ctrl : MonoBehaviour
+
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y--;
+            this.transform.position = position;
+        }
+    }
+}
