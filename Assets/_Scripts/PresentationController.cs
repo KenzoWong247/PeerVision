5af45391006f5b0695ea3fc154e539843593d1db
Draper <panirwalak@wit.edu>
2018-08-03 09:01:36 -0700

    Networking changes

--- Assets/_Scripts/PresentationController.cs
+++ Assets/_Scripts/PresentationController.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using System.Linq;   public class PresentationController : MonoBehaviour
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using HTC.UnityPlugin.Vive; using System.Linq;   public class PresentationController : MonoBehaviour
 {     public int i;     public Texture2D[] textures = null;     private SpriteRenderer sp;
 
-    private SteamVR_TrackedObject trackedObj;
-
-    // 2
-    private SteamVR_Controller.Device Controller
-    {
-        get { return SteamVR_Controller.Input((int)trackedObj.index); }
-    }
-
-    void Awake()
-    {
-        trackedObj = GetComponent<SteamVR_TrackedObject>();
-    }
+    //private SteamVR_TrackedObject trackedObj;
+    //
+    //// 2
+    //private SteamVR_Controller.Device Controller
+    //{
+    //    get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    //}
+    //
+    //void Awake()
+    //{
+    //    trackedObj = GetComponent<SteamVR_TrackedObject>();
+    //}
 
 
     // Use this for initialization
@@ -24,8 +24,8 @@
 
     // Update is called once per frame
     void Update()
-    {         if (Controller.GetHairTriggerDown())
+    {         if (ViveInput.GetPressDown(HandRole.RightHand, ControllerButton.Trigger))
         {             i++;             if (i < textures.Length)
             {                 sp.sprite = Sprite.Create(textures[i], new Rect(0.0f, 0.0f, textures[i].width, textures[i].height), new Vector2(0.5f, 0.5f), 425.0f);             }
             else
-            {                 sp.sprite = Sprite.Create(textures[textures.Length - 1], new Rect(0.0f, 0.0f, textures[textures.Length - 1].width, textures[textures.Length - 1].height), new Vector2(0.5f, 0.5f), 425.0f);             }         }          if (Controller.GetHairTriggerDown())         {             i--;             if (i >= 0)             {                 sp.sprite = Sprite.Create(textures[i], new Rect(0.0f, 0.0f, textures[i].width, textures[i].height), new Vector2(0.5f, 0.5f), 425.0f);             }             else             {                 sp.sprite = Sprite.Create(textures[0], new Rect(0.0f, 0.0f, textures[0].width, textures[0].height), new Vector2(0.5f, 0.5f), 425.0f);             }         }     } }  
+            {                 sp.sprite = Sprite.Create(textures[textures.Length - 1], new Rect(0.0f, 0.0f, textures[textures.Length - 1].width, textures[textures.Length - 1].height), new Vector2(0.5f, 0.5f), 425.0f);             }         }          if (ViveInput.GetPressDown(HandRole.LeftHand, ControllerButton.Trigger))         {             i--;             if (i >= 0)             {                 sp.sprite = Sprite.Create(textures[i], new Rect(0.0f, 0.0f, textures[i].width, textures[i].height), new Vector2(0.5f, 0.5f), 425.0f);             }             else             {                 sp.sprite = Sprite.Create(textures[0], new Rect(0.0f, 0.0f, textures[0].width, textures[0].height), new Vector2(0.5f, 0.5f), 425.0f);             }         }     } }  
