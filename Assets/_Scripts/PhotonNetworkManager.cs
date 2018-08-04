0f3f5f081790d08fbee328c4649dfc5e828a5958
KenzoWong247 <kenzo.wong247@gmail.com>
2018-08-04 19:56:50 -0400

    Fixed movement over network

---
+++ Assets/_Scripts/PhotonNetworkManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonNetworkManager : Photon.MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform[] spawnPoints;
+
+    private int maxSpawnpoints;
+    private int numberOfPlayers;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        string version = "Version 0.1";
+        PhotonNetwork.ConnectUsingSettings(version);
+        numberOfPlayers = 0;
+        maxSpawnpoints = spawnPoints.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public virtual void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+    }
+
+    public virtual void OnJoinedLobby()
+    {
+        Debug.Log("We have now joined the lobby");
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 6;
+
+        PhotonNetwork.JoinOrCreateRoom("RoomName", roomOptions, null);
+        
+    }
+
+    public virtual void OnJoinedRoom()
+    {
+        int numberOfPlayers = PhotonNetwork.playerList.Length;
+        if (numberOfPlayers > 2)
+            numberOfPlayers = numberOfPlayers % 2;
+        int spawnpoint = numberOfPlayers - 1;
+
+        Debug.Log("Spawnpoint: " + spawnpoint);
+        PhotonNetwork.Instantiate(player.name, spawnPoints[spawnpoint].position, spawnPoints[spawnpoint].rotation, 0);
+    }
+    
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public int speed;
+
+    [SerializeField]
+    public int rotationSpeed;
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal*speed*Time.deltaTime, 0.0f, moveVertical*speed*Time.deltaTime);
+
+        transform.position += movement * Time.fixedDeltaTime;
+        transform.Rotate(0, Input.GetAxis("Rotate")*rotationSpeed*Time.deltaTime, 0); 
+        
+    }
+}
