746d161268cb200ae5cee97f33c463724d0973fe
KenzoWong247 <kenzo.wong247@gmail.com>
2018-08-04 21:10:35 -0400

    Fixed client moving both characters

--- Assets/_Scripts/PlayerNetwork.cs
+++ Assets/_Scripts/PlayerNetwork.cs
@@ -3,6 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerNetwork : MonoBehaviour {
-     
-    //[SerializeField] 
+    
+    [SerializeField] private MonoBehaviour[] playerControlScripts;
+
+    [SerializeField] private GameObject playerEars;
+
+    [SerializeField] private GameObject playerEyes;
+
+    private PhotonView photonView;
+
+    private void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+
+        Initialize();
+    }
+    
+    private void Initialize()
+    {
+        if (!photonView.isMine)
+        {
+            playerEars.SetActive(false);
+            playerEyes.SetActive(false);
+            foreach(MonoBehaviour m in playerControlScripts)
+            {
+                m.enabled = false;
+            }
+        }
+    }
 }
