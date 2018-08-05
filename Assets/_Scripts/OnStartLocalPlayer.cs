b9e980bb97d5e4ca685669756e294ce507748da5
KenzoWong247 <kenzo.wong247@gmail.com>
2018-08-05 13:35:52 -0400

    Changed tracked objects for HTC Vive

---
+++ Assets/_Scripts/OnStartLocalPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnStartLocalPlayer : MonoBehaviour {
+
+    [SerializeField] public Transform RigHead;
+    [SerializeField] public Transform RigRightHand;
+    [SerializeField] public Transform RigLeftHand;
+    [SerializeField] public Transform PlayerHead;
+    [SerializeField] public Transform PlayerRightHand;
+    [SerializeField] public Transform PlayerLeftHand;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerHead.position = RigHead.position;
+        PlayerRightHand.position = RigRightHand.position;
+        PlayerLeftHand.position = RigLeftHand.position;
+        
+	}
+}
